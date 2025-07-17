076394235e9a8ab6ede320338c5420d364ba73b5
Dmouse1101 <anandaniansh@gmail.com>
2025-07-17 17:50:13 +0530

    update

--- src/routes/sweetRouter.js
+++ src/routes/sweetRouter.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/sweetController");
-const upload = require("../middlewares/upload.js");
 
 router.post("/", controller.add);
 router.get("/", controller.list);
